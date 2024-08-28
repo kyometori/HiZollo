f86d8cad9fad4b68895c33510e1a265edd617566
Zollo757347 <81338417+Zollo757347@users.noreply.github.com>
2024-08-28 10:12:35 +0800

    feature: more detailed channel info

--- src/commands/Server.ts
+++ src/commands/Server.ts
@@ -18,9 +18,10 @@
  * along with Junior HiZollo. If not, see <https://www.gnu.org/licenses/>.
  */
 
-import { ChannelType, EmbedBuilder, PermissionFlagsBits } from "discord.js";
+import { ChannelType, Collection, GuildBasedChannel } from "discord.js";
+import { EmbedBuilder, PermissionFlagsBits } from "discord.js";
+import type { Source } from "../classes/Source";
 import { Command } from "../classes/Command";
-import { Source } from "../classes/Source";
 import { CommandType } from "../typings/enums";
 
 export default class Server extends Command<[]> {
@@ -46,12 +47,6 @@ export default class Server extends Command<[]> {
     }
     
     await source.defer();
-    const owner = await guild.fetchOwner();
-    const channels = {
-      textCount: guild.channels.cache.filter(c => c.type === ChannelType.GuildText && !c.isThread()).size,
-      voiceCount: guild.channels.cache.filter(c => c.type === ChannelType.GuildVoice).size,
-      threadCount: guild.channels.cache.filter(c => c.isThread()).size,
-    };
 
     const info = new EmbedBuilder()
       .applyHiZolloSettings(source.member, `${guild.name} 的伺服器資訊`)
@@ -59,11 +54,54 @@ export default class Server extends Command<[]> {
       .addFields(
         { name: '伺服器 ID', value: guild.id },
         { name: 'HiZollo 分支編號', value: client.shard?.ids[0] === undefined ? '查無資訊' : `${client.shard?.ids[0]}`},
-        { name: '擁有者', value: `<@${owner.user.id}>` },
+        { name: '擁有者', value: `<@${guild.ownerId}>` },
         { name: '成員數量', value: `${guild.memberCount}` },
-        { name: '頻道數量', value: `文字頻道：${channels.textCount}\n語音頻道：${channels.voiceCount}\n討論串：${channels.threadCount}` },
+        { name: '頻道數量', value: this.parseChannelCount(guild.channels.cache) },
         { name: '創立時間', value: `<t:${~~(guild.createdTimestamp/1000)}>` }
       );
     await source.update({ embeds: [info] });
+  }
+
+  private parseChannelCount(channels: Collection<string, GuildBasedChannel>): string {
+    let text = 0, voice = 0, stage = 0, announcement = 0, forum = 0, thread = 0;
+    channels.forEach(({ type, name }) => {
+      switch (type) {
+        case ChannelType.GuildText:
+          console.log(name);
+          text++;
+          break;
+        
+        case ChannelType.GuildVoice:
+          voice++;
+          break;
+        
+        case ChannelType.GuildStageVoice:
+          stage++;
+          break;
+
+        case ChannelType.GuildAnnouncement:
+          announcement++;
+          break;
+        
+        case ChannelType.GuildForum:
+          forum++;
+          break;
+        
+        case ChannelType.PublicThread:
+        case ChannelType.AnnouncementThread:
+          thread++;
+          break;
+      }
+    });
+
+    let result = '';
+    if (text) result += `文字頻道：${text}\n`;
+    if (voice) result += `語音頻道：${voice}\n`;
+    if (stage) result += `舞台頻道：${stage}\n`;
+    if (announcement) result += `公告頻道：${announcement}\n`;
+    if (forum) result += `論壇頻道：${forum}\n`;
+    if (thread) result += `公開討論串：${thread}\n`;
+    if (!result) result = "本伺服器沒有任何頻道";
+    return result;
   }
 }
